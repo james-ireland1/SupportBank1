afc6301f6f9911134d7cbf15c6654b29113d57cd
James Ireland <james.ireland@softwire.com>
2025-06-18 15:42:44 +0100

    First pass at the problem

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,71 @@
 "use strict";
-console.log("Hello");
+const fs = require('node:fs');
+class Accounts {
+    constructor() {
+        this.people = [];
+    }
+    addPerson(name) {
+        this.people.push(new Person(name));
+        return this.people[this.people.length - 1];
+    }
+    getPerson(name) {
+        for (let person of this.people) {
+            if (person.name === name) {
+                return person;
+            }
+        }
+        return this.addPerson(name);
+    }
+    addTransaction(date, nameFrom, nameTo, narrative, amount) {
+        let personFrom;
+        personFrom = this.getPerson(nameFrom);
+        let personTo;
+        personTo = this.getPerson(nameTo);
+        let transaction = new Transaction(date, personFrom, personTo, narrative, amount);
+        personFrom.addTransaction(transaction);
+        personTo.addTransaction(transaction);
+    }
+}
+class Person {
+    constructor(name) {
+        this.name = name;
+        this.balance = 0;
+        this.transactions = [];
+    }
+    addTransaction(transaction) {
+        this.transactions.push(transaction);
+        if (this.name === transaction.personFrom.name) {
+            this.balance -= transaction.amount;
+        }
+        else {
+            this.balance += transaction.amount;
+        }
+    }
+}
+class Transaction {
+    constructor(date, personFrom, personTo, narrative, amount) {
+        this.date = date;
+        this.personFrom = personFrom;
+        this.personTo = personTo;
+        this.narrative = narrative;
+        this.amount = Number(amount);
+    }
+}
+let fullAccount = new Accounts();
+fs.readFile('Transactions2014.csv', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    processData(data);
+});
+function processData(textData) {
+    let lines = textData.split(/\r?\n/);
+    lines.splice(0, 1);
+    for (let line of lines) {
+        let parts = line.split(',');
+        fullAccount.addTransaction(parts[0], parts[1], parts[2], parts[3], parts[4]);
+    }
+    console.log(fullAccount);
+}
 //# sourceMappingURL=index.js.map
